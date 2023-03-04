f65a94a1397333adb81169c927397247e841582b
Shavlego Botchorishvili <shavlego.dev@gmail.com>
2023-03-04 17:58:57 +0400

    created necessary files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplatesComponent } from './pages/templates/templates.component';
+import { TemplateComponent } from './pages/template/template.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplatesComponent,
+    TemplateComponent
   ],
   imports: [
     BrowserModule,
